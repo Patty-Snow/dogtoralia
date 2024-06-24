02fc358d62279c1dd62a9329e35febf87d328f83
Beltran22 <diego.beltran@devitm.com>
2024-06-24 17:57:34 -0600

    Migraciones de businesses y stylists

---
+++ database/migrations/2024_06_24_234254_create_stylists_veterinarians_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStylistsVeterinariansTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('stylists_veterinarians', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 45);
+            $table->string('last_name', 45);
+            $table->string('surname', 45);
+            $table->string('email', 70)->unique();
+            $table->string('password', 255);
+            $table->timestamp('registration_date');
+            $table->text('profile_photo');
+            $table->foreignId('business_id')->constrained('businesses'); 
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('stylists_veterinarians');
+    }
+}
